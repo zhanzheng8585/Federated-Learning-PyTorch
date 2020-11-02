872ca11f2ced8bf2c3c82fdf3d44a4b6dca9d346
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-11-01 19:06:17 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 
 # python src/baseline_main.py --model=mlp --dataset=mnist --gpu=0 --epochs=10
-python src/federated_main.py --model=cnn --dataset=cifar --gpu=0 --iid=1 --epochs=10
+python src/federated_main.py --model=cnn --dataset=cifar --gpu=0 --iid=1 --epochs=50
 
