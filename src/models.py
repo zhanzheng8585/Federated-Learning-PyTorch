f4ffc51e178d89da55715855fd037cdb9bb9c50a
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-11-01 23:15:56 -0500

    v1

--- src/models.py
+++ src/models.py
@@ -27,11 +27,11 @@ class MLP(nn.Module):
 class CNNMnist(nn.Module):
     def __init__(self, args):
         super(CNNMnist, self).__init__()
-        self.conv1 = nn.Conv2d(args.num_channels, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(args.num_channels, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, args.num_classes)
+        self.fc1 = nn.Linear(512, 512)
+        self.fc2 = nn.Linear(512, args.num_classes)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
