b93a6350bc2ffdfdaa5680371940b81dd5fb8ce6
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-11-01 19:55:41 -0500

    v1

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=7
 
 # python src/baseline_main.py --model=mlp --dataset=mnist --gpu=0 --epochs=10
-python src/federated_main.py --model=cnn --dataset=cifar --gpu=0 --iid=1 --epochs=50
+python src/federated_main.py --model=cnn --dataset=cifar --gpu --gpu_id=0 --iid=1 --epochs=50
 
