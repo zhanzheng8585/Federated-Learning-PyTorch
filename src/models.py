42bb736750a77c602b2f63318e9b07a19153750c
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-11-12 20:03:58 -0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -69,17 +69,17 @@ class CNNFashion_Mnist(nn.Module):
 class CNNCifar(nn.Module):
     def __init__(self, args):
         super(CNNCifar, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(64, 128, 5)
-        self.fc1 = nn.Linear(128 * 5 * 5, 256)
+        self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
+        self.fc1 = nn.Linear(128 * 8 * 8, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, args.num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 128 * 5 * 5)
+        x = x.view(-1, 128 * 8 * 8)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
