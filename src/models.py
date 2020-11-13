090bbbe0d8369f8e20cfe2db12ba9d5a1c7e48e2
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-11-12 19:55:59 -0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -72,14 +72,14 @@ class CNNCifar(nn.Module):
         self.conv1 = nn.Conv2d(3, 64, 3)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(64, 128, 3)
-        self.fc1 = nn.Linear(128 * 8 * 8, 256)
+        self.fc1 = nn.Linear(128 * 7 * 7, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, args.num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 128 * 8 * 8)
+        x = x.view(-1, 128 * 7 * 7)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
