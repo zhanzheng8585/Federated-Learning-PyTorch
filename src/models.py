0fa07fb1dd64986d6553c1fd2dfbfaa209074526
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-11-13 11:38:32 -0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -70,9 +70,9 @@ class CNNFashion_Mnist(nn.Module):
 class CNNCifar(nn.Module):
     def __init__(self, args):
         super(CNNCifar, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
+        self.conv1 = nn.Conv2d(3, 64, 3, padding=1, bias=False)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, 3, padding=1, bias=False)
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         self.classifier = nn.Sequential(
             nn.Linear(128 * 7 * 7, 512),
