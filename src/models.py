be505d6eadb01bceebbb809b3e604f4c76a0b733
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-11-13 19:50:25 -0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -75,15 +75,15 @@ class CNNCifar(nn.Module):
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(64, 128, 3, padding=1, bias=False)
         # self.bn2 = nn.BatchNorm2d(128)
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.avgpool = nn.AdaptiveAvgPool2d((3, 3))
         self.classifier = nn.Sequential(
-            nn.Linear(128 * 7 * 7, 144),
+            nn.Linear(128 * 3 * 3, 512),
             nn.ReLU(True),
             nn.Dropout(),
-            nn.Linear(144, 144),
+            nn.Linear(512, 512),
             nn.ReLU(True),
             nn.Dropout(),
-            nn.Linear(144, args.num_classes),
+            nn.Linear(512, args.num_classes),
         )
         # self.fc1 = nn.Linear(128 * 8 * 8, 256)
         # self.fc2 = nn.Linear(256, 128)
